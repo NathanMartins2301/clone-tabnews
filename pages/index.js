7665093edcd19ce237c5c9985de93fa13b5d8573
NathanMartins2301 <nathan.carneiro23011@gmail.com>
2023-11-15 14:03:34 -0300

    change page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Mensagem legal!</h1>
+    return (
+        <>
+            <h1>Mensagem legal! </h1>
+            <span>Aprendendo sobre CI/CD</span>
+        </>
+    )
 }
 
 export default Home;
