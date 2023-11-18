f8b9a3dd6b5171cb183655a719c22240acf9b8db
NathanMartins2301 <nathan.carneiro23011@gmail.com>
2023-11-18 11:18:28 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 function Home() {
-    return (
-        <>
-            <h1>Mensagem legal! </h1>
-            <span>Aprendendo sobre CI/CD</span>
-        </>
-    )
+  return (
+    <>
+      <h1>Mensagem legal! </h1>
+      <span>Aprendendo sobre CI/CD</span>
+    </>
+  );
 }
 
 export default Home;
